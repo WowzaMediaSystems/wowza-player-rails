56a2d80b33961c19a8dc5dcb2c53afdf6158823a
Drew Mitchell <drew.mitchell@wowza.com>
2016-12-28 09:39:28 -0700

    adding style param

--- lib/wowza_player/view_helpers.rb
+++ lib/wowza_player/view_helpers.rb
@@ -7,8 +7,10 @@ module WowzaPlayer
     end
 
     def wowza_player_tag(config={})
+      style = config.has_key?(:container_style) ? "style='#{config.delete(:container_style)}'" : ""
+
       unsafe_html = <<-EOS
-      <div id="wowza_player"></div>
+      <div id="wowza_player" #{style}></div>
       <script type="text/javascript">
         var config = #{JSON.pretty_generate(config)};
         WowzaPlayer.create('wowza_player',config);
